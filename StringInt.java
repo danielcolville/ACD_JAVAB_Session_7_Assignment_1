93b61fa11b435997244009ded971ce9e1863e3dc
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-13 15:59:22 -0400

    Add files via upload

---
+++ StringInt.java
@@ -0,0 +1,11 @@
+package session7;
+
+public class StringInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		System.out.println(String.valueOf(x));
+	}
+
+}
